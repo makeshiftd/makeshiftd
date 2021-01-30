46c2600ee995706c9136378ad4f174f7e1795bc5
Dylan Maxwell <dylanxmaxwell@gmail.com>
2021-01-29 20:33:44 -0500

    Add basic logging config and simple main func

--- cmd/makeshiftd/main.go
+++ cmd/makeshiftd/main.go
@@ -2,16 +2,55 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"net"
 	"net/http"
 	"os"
 	"os/signal"
 	"time"
 
+	"github.com/mattn/go-isatty"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+
 	wg "github.com/dxmaxwell/workgroup"
 )
 
-func listenAndServe(ctx context.Context, h http.Handler) {
+func main() {
+
+	if isatty.IsTerminal(os.Stdout.Fd()) {
+		w := zerolog.NewConsoleWriter(func(w *zerolog.ConsoleWriter) {
+			w.Out = os.Stdout
+		})
+		log.Logger = zerolog.New(w).With().Timestamp().Logger()
+	}
+
+	err := mainWithContext(context.Background())
+	if err != nil {
+		log.Err(err)
+		if err, ok := err.(interface{ ExitCode() int }); ok {
+			os.Exit(err.ExitCode())
+		}
+		os.Exit(1)
+	}
+}
+
+func mainWithContext(ctx context.Context) error {
+
+	log.Info().Msg("Makeshiftd starting")
+
+	handler := http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+		fmt.Printf("Hello World!")
+		res.Write([]byte("<html><body>Hello World</body></html>"))
+	})
+
+	err := listenAndServe(ctx, handler)
+
+	log.Info().Msg("Makshiftd stopped")
+	return err
+}
+
+func listenAndServe(ctx context.Context, h http.Handler) error {
 	if ctx == nil {
 		ctx = context.TODO()
 	}
@@ -28,7 +67,7 @@ func listenAndServe(ctx context.Context, h http.Handler) {
 		},
 	}
 
-	wg.Work(
+	return wg.Work(
 		context.Background(),
 		wg.NewUnlimited(),
 		wg.CancelOnFirstError(),
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/makeshiftd/makeshiftd
 
 go 1.16
 
-require github.com/dxmaxwell/workgroup v0.0.0-20210126012021-bfde0375429d
+require (
+	github.com/dxmaxwell/workgroup v0.0.0-20210126012021-bfde0375429d
+	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/rs/zerolog v1.20.0 // indirect
+)
